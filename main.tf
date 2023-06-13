ba8667ac8bc4ccf98053bb8c4f0d5d90eb0aebad
Chirag Bhatti <chirag.bhatti@softwire.com>
2023-06-13 16:55:44 +0100

    feat: part2 - terraform

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~>3.0"
+      source  = "hashicorp/azurerm"
+      version = ">=3.43.0"
     }
   }
 }
@@ -11,14 +11,53 @@ provider "azurerm" {
   features {}
 }
 
-provider "azurerm" {
-  features {}
+data "azurerm_resource_group" "main" {
+  name = "Cohort25_ChiBha_Workshop_M12_Pt2"
 }
 
-
-
-data "azurerm_linux_web_app" "example" {
-  name                = "to-do-app-kl"
-  resource_group_name = "LV21_KeithLeverton_ProjectExercise"
+resource "azurerm_service_plan" "main" {
+  name                = "chibha-terraformed-asp"
+  resource_group_name = data.azurerm_resource_group.main.name
+  location            = data.azurerm_resource_group.main.location
+  os_type             = "Linux"
+  sku_name            = "B1"
 }
 
+resource "azurerm_linux_web_app" "example" {
+  name                = "chibha-terraformed-app"
+  resource_group_name = data.azurerm_resource_group.main.name
+  location            = azurerm_service_plan.main.location
+  service_plan_id     = azurerm_service_plan.main.id
+
+  site_config {
+    application_stack {
+      docker_image     = "corndeldevopscourse/mod12app"
+      docker_image_tag = "latest"
+    }
+  }
+
+  app_settings = {
+    "CONNECTION_STRING" = "Server=tcp:chibha-non-iac-sqlserver.database.windows.net,1433;Initial Catalog=chibha-non-iac-db;Persist Security Info=False;User ID=dbadmin;Password=${var.database_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;",
+    "DEPLOYMENT_METHOD" = "Terraform",
+    "SCM_DO_BUILD_DURING_DEPLOYMENT" : "True"
+  }
+}
+
+resource "azurerm_mssql_server" "main" {
+  name                         = "chibha-non-iac-sqlserver"
+  resource_group_name          = data.azurerm_resource_group.main.name
+  location                     = data.azurerm_resource_group.main.location
+  version                      = "12.0"
+  administrator_login          = "dbadmin"
+  administrator_login_password = var.database_password
+}
+
+resource "azurerm_mssql_database" "main" {
+  name           = "chibha-non-iac-db"
+  server_id      = azurerm_mssql_server.main.id
+  sku_name       = "Basic"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
